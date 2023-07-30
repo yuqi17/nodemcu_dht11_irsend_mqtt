c036a22c55fe41e954fc7c802c7e26fcd24416c2
yuqi <yu.qi@mwee.cn>
2023-07-30 14:38:25 +0800

    feedback

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 
-//D4 GPIO2 LED_BUILTIN 会引起不停闪烁
-//D8 GPIO15 会引起连接不上nodemcu
+// D4 GPIO2 LED_BUILTIN 会引起不停闪烁
+// D8 GPIO15 会引起连接不上nodemcu
 
 #include <ESP8266WiFi.h>
 #include <DNSServer.h>
@@ -17,21 +17,22 @@
 #include <SimpleDHT.h>
 
 // 引脚设置
-#define IR_LED 4  // D2 GPIO 4
+#define IR_LED 4 // D2 GPIO 4
 IRCoolixAC coolix(IR_LED);
 
-#define DHT_PIN 12  // D6 GPIO12 
+#define DHT_PIN 12 // D6 GPIO12
 SimpleDHT11 dht11(DHT_PIN);
 
 // 网络和MQTT 设置
 WiFiClient espClient;
-const char* mqtt_server = "broker.emqx.io";
+const char *mqtt_server = "broker.emqx.io";
 const int mqtt_port = 1883;
-const char* mqtt_user = "jonas";
-const char* mqtt_password = "abc123";
+const char *mqtt_user = "jonas";
+const char *mqtt_password = "abc123";
 PubSubClient client(espClient);
-const char* mqttSubscribeTopic = "switch";
-const char* mqttSubscribeTopic1 = "weather";
+const char *switchSubscribeTopic = "switch";
+const char *weatherPublishTopic = "weather";
+const char *switchPublishTopic = "switch/feedback";
 
 // 定时器回调设置
 Ticker publishDataTicker;
@@ -40,10 +41,10 @@ Ticker subscribeDataTicker;
 // 定义定时器回调函数 定义之后就可以把方法写在后面的位置
 void publishDataTask();
 void subscribeDataTask();
-void callback(char* topic, byte* payload, unsigned int length);
+void callback(char *topic, byte *payload, unsigned int length);
 
-
-void setup() {
+void setup()
+{
 
   // pinMode(LED_BUILTIN, OUTPUT); 不设置默认的低电平会让led 一直亮
   Serial.begin(115200);
@@ -57,12 +58,16 @@ void setup() {
   client.setCallback(callback);
   // client.setKeepAlive(20); // 默认是15s 心跳
 
-  while (!client.connected()) {
+  while (!client.connected())
+  {
     Serial.println("连接MQTT服务器中...");
     // mqttClient.connect(clientId.c_str(), mqttId, mqttPwd, willTopic, willQoS, willRetain, willMsg)；
-    if (client.connect("完全新的的clientId", mqtt_user, mqtt_password)) {
+    if (client.connect("完全新的的clientId", mqtt_user, mqtt_password))
+    {
       Serial.println("连接MQTT成功");
-    } else {
+    }
+    else
+    {
       Serial.print("连接失败： ");
       Serial.print(client.state());
       delay(2000);
@@ -73,20 +78,23 @@ void setup() {
   publishDataTicker.attach_ms(20000, publishDataTask); // 温度湿度 20s 发布一次
 
   // 订阅 MQTT 主题
-  client.subscribe(mqttSubscribeTopic);
+  client.subscribe(switchSubscribeTopic);
   // 这个会关联 callback => 影响IR 发射器的响应时间
-  subscribeDataTicker.attach_ms(200, subscribeDataTask);  
+  subscribeDataTicker.attach_ms(200, subscribeDataTask);
 }
 
-
-void callback(char* topic, byte* payload, unsigned int length) {
+void callback(char *topic, byte *payload, unsigned int length)
+{
   Serial.println("Message arrived:");
   Serial.println(topic);
-  if (strcmp(topic, mqttSubscribeTopic) == 0) {
+  if (strcmp(topic, switchSubscribeTopic) == 0)
+  {
     String message;
-    for (unsigned int i = 0; i < length; i++) {
+    for (unsigned int i = 0; i < length; i++)
+    {
       message += (char)payload[i];
     }
+
     Serial.println(message);
 
     // 将json 字符串解析为json 对象并取出信息
@@ -95,26 +103,32 @@ void callback(char* topic, byte* payload, unsigned int length) {
     JsonObject obj = doc.as<JsonObject>();
     String state = obj["state"];
 
-    Serial.println(state);
-
-    if (state.equals("on")) {
+    if (state.equals("on"))
+    {
       coolix.on();
       coolix.send();
-    } else if (state.equals("off")) {
+      const char *feedback = "openned";
+      client.publish(switchPublishTopic, feedback, false);
+    }
+    else if (state.equals("off"))
+    {
       coolix.off();
       coolix.send();
+      const char *feedback = "shut down";
+      client.publish(switchPublishTopic, feedback, false);
     }
   }
 }
 
-
 // 定时器回调函数，发布温湿度数据到 MQTT
-void publishDataTask() {
+void publishDataTask()
+{
   // 读取温湿度数据
   byte temperature = 0;
   byte humidity = 0;
   int err = SimpleDHTErrSuccess;
-  if ((err = dht11.read(&temperature, &humidity, NULL)) != SimpleDHTErrSuccess) {
+  if ((err = dht11.read(&temperature, &humidity, NULL)) != SimpleDHTErrSuccess)
+  {
     Serial.print("温湿度传感器异常:");
     Serial.print(SimpleDHTErrCode(err));
     Serial.print(",");
@@ -131,14 +145,16 @@ void publishDataTask() {
   char sendJson[256];
   serializeJson(data, sendJson);
   // 发布温湿度数据到 MQTT
-  client.publish(mqttSubscribeTopic1, sendJson, false);
+  client.publish(weatherPublishTopic, sendJson, false);
 }
 
 // 定时器回调函数，检查 MQTT 订阅的消息
-void subscribeDataTask() {
+void subscribeDataTask()
+{
   // 检查 MQTT 订阅的消息
   client.loop();
 }
 
-void loop() {
+void loop()
+{
 }
