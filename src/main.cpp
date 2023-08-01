cc32380602a5bb4b5d33e5f043f474b3bc13362d
yuqi <yu.qi@mwee.cn>
2023-08-02 00:20:49 +0800

    fix

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 #include <ESP8266WiFi.h>
 #include <DNSServer.h>
 #include <ESP8266WebServer.h>
+#include <WiFiUdp.h>
 
 // 第三方库
 #include <ArduinoJson.h>
@@ -34,14 +35,21 @@ const char *switchSubscribeTopic = "switch";
 const char *weatherPublishTopic = "weather";
 const char *switchPublishTopic = "switch/feedback";
 
+WiFiUDP Udp;                       // 实例化WiFiUDP对象
+unsigned int localUdpPort = 1234;  // 自定义本地监听端口
+unsigned int remoteUdpPort = 4321; // 自定义远程监听端口
+char incomingPacket[255];  // 保存Udp工具发过来的消息
+
 // 定时器回调设置
 Ticker publishDataTicker;
 Ticker subscribeDataTicker;
+Ticker udpTicker;
 
 // 定义定时器回调函数 定义之后就可以把方法写在后面的位置
 void publishDataTask();
 void subscribeDataTask();
 void callback(char *topic, byte *payload, unsigned int length);
+void udpDataTask();
 
 void setup()
 {
@@ -93,10 +101,69 @@ void setup()
   client.subscribe(switchSubscribeTopic);
   // 这个会关联 callback => 影响IR 发射器的响应时间
   subscribeDataTicker.attach_ms(200, subscribeDataTask);
+
+  if (Udp.begin(localUdpPort))
+  { // 启动Udp监听服务
+    Serial.println("监听成功");
+    // 打印本地的ip地址，在UDP工具中会使用到
+    // WiFi.localIP().toString().c_str()用于将获取的本地IP地址转化为字符串
+    Serial.printf("现在收听IP：%s, UDP端口：%d\n", WiFi.localIP().toString().c_str(), localUdpPort);
+  }
+  else
+  {
+    Serial.println("监听失败");
+  }
+
+  udpTicker.attach_ms(200, udpDataTask);
   // Serial.println("deep sleep for 60 seconds");
   // ESP.deepSleep(60e6); //行不通
   // ESP.deepSleep(0);
 }
+
+
+//向udp工具发送消息
+void sendCallBack(const char *buffer)
+{  
+  Udp.beginPacket(Udp.remoteIP(), remoteUdpPort);//配置远端ip地址和端口
+  Udp.write(buffer); //把数据写入发送缓冲区
+  Udp.endPacket(); //发送数据 
+}
+
+void udpDataTask(){
+  int packetSize = Udp.parsePacket();//获得解析包
+  if (packetSize)//解析包不为空
+  {
+    //收到Udp数据包
+    //Udp.remoteIP().toString().c_str()用于将获取的远端IP地址转化为字符串
+    Serial.printf("收到来自远程IP：%s（远程端口：%d）的数据包字节数：%d\n", Udp.remoteIP().toString().c_str(), Udp.remotePort(), packetSize);
+      
+    // 读取Udp数据包并存放在incomingPacket
+    int len = Udp.read(incomingPacket, 255);//返回数据包字节数
+    if (len > 0)
+    { 
+      incomingPacket[len] = 0;//清空缓存
+      Serial.printf("UDP数据包内容为: %s\n", incomingPacket);//向串口打印信息
+ 
+      //strcmp函数是string compare(字符串比较)的缩写，用于比较两个字符串并根据比较结果返回整数。
+      //基本形式为strcmp(str1,str2)，若str1=str2，则返回零；若str1<str2，则返回负数；若str1>str2，则返回正数。
+      if (strcmp(incomingPacket, "LED_OFF") == 0) // 命令LED_OFF  
+      {  
+        digitalWrite(LED_BUILTIN, HIGH); // 熄灭LED  
+        sendCallBack("LED has been turn off\n");  
+      }  
+      else if (strcmp(incomingPacket, "LED_ON") == 0) //如果收到LED_ON  
+      {  
+        digitalWrite(LED_BUILTIN, LOW); // 点亮LED  
+        sendCallBack("LED has been turn on\n");  
+      }  
+      else // 如果指令错误，调用sendCallBack
+      {  
+        sendCallBack("Command Error!");  
+      }  
+    }
+  }
+}
+
 
 void callback(char *topic, byte *payload, unsigned int length)
 {
@@ -137,7 +204,7 @@ void callback(char *topic, byte *payload, unsigned int length)
 // 定时器回调函数，发布温湿度数据到 MQTT
 void publishDataTask()
 {
-  Serial.println("温度湿度");
+  
   // 读取温湿度数据
   byte temperature = 0;
   byte humidity = 0;
@@ -161,6 +228,8 @@ void publishDataTask()
   serializeJson(data, sendJson);
   // 发布温湿度数据到 MQTT
   client.publish(weatherPublishTopic, sendJson, false);
+
+  sendCallBack(sendJson);
 }
 
 // 定时器回调函数，检查 MQTT 订阅的消息
